dbbc443033b11d76b71b514988d2a60c02c9a2cd
ashan1995 <ashan.samarasinghe95@gmail.com>
2021-04-16 20:45:38 +0530

    feature updated

---
+++ viewprofile.php
@@ -0,0 +1,63 @@
+<?php
+include('layout/header.php');
+include('layout/navbar.php');
+include('layout/sidebar.php');
+?>
+
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Profile</h3>
+            </div>
+            <!-- /.card-header -->
+            <!-- form start -->
+              <div class="card-body">
+                <div class="form-group">
+                  <label>Name</label>
+                  <h3>John Doe</h3>
+                </div>
+                <div class="form-group">
+                  <label>Employee ID</label>
+                  <h3>007</h3>
+                </div>
+                <div class="form-group">
+                  <label >Supervisor</label>
+                  <h3>Jane Doe</h3>
+                </div>
+                <div class="form-group">
+                  <label>Email</label>
+                  <h3>johndoe@gmail.com</h3>
+                </div>
+                <div class="form-group">
+                  <label>Department</label>
+                  <h3>HR</h3>
+                </div>
+                <div class="form-group">
+                  <label>Location</label>
+                  <h3>Colombo</h3>
+                </div>
+
+              </div>
+              <!-- /.card-body -->
+
+              
+           
+          </div>
+        </div>
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+</div>
+
+
+
+
+<?php
+include('layout/scripts.php');
+include('layout/footer.php');
+?>
